c9384690f6e769260f71b42af069c83168e3bff6
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-09 17:26:09 +0200

    Add the first Service

---
+++ FirstService/FirstService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IService;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace FirstService
+{
+    public class FirstService : IService.IService
+    {
+        // A dictionary to store the all the UEFA Champions League winners. 
+
+        Dictionary<string, string> winners;
+
+        FirstService()
+        {
+            // very odd to do this inside the constructor , but it's all about the load balancer not this service...
+            try
+            {
+                using (StreamReader file = new StreamReader("Resources/UEFA.json"))
+                {
+                    string winnersJson = file.ReadToEnd();
+                    winners = JsonConvert.DeserializeObject<Dictionary<string, string>>(winnersJson);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("An Exception occured : " + exception.Message);
+            }
+        }
+
+        public string ChampWinner(long year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FirstService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/FirstService");
+
+            using (ServiceHost host = new ServiceHost(typeof(FirstService), baseAddress))
+            {
+                // Enable metadata publishing.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(smb);
+
+                // Open the ServiceHost to start listening for messages ( this is a self-hosted service )
+                host.Open();
+
+                Console.WriteLine("The service is ready at {0}", baseAddress);
+                Console.WriteLine("Press <enter> to stop the service.");
+                Console.ReadLine();
+
+                host.Close();
+            }
+
+        }
+    }
+}
