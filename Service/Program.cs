5d377a5dd59b3ec70fe6a426c1ea2b1841fe2320
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-09 17:00:56 +0200

    move solution file to root folder and create the service

---
+++ Service/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] arguments = Environment.GetCommandLineArgs();
+            Uri baseAddress = new Uri("http://localhost:" + arguments[1] + "/Service");
+
+            using (ServiceHost host = new ServiceHost(typeof(Service), baseAddress))
+            {
+                // Enable metadata publishing.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(smb);
+
+                // Open the ServiceHost to start listening for messages ( this is a self-hosted service )
+                host.Open();
+
+                Console.WriteLine("The service is ready at {0}", baseAddress);
+                Console.WriteLine("Press <enter> to stop the service.");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+    }
+}
