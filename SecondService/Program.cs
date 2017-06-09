dcd2fc7b1dffc76a19f91d2d30e482e0869b2b1c
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-09 17:35:53 +0200

    Add the second service

---
+++ SecondService/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Uri baseAddress = new Uri("http://localhost:8080/SecondService");
+
+             using (ServiceHost host = new ServiceHost(typeof(SecondService), baseAddress))
+             {
+                 // Open the ServiceHost to start listening for messages ( this is a self-hosted service )
+                 host.Open();
+
+                 Console.WriteLine("The service is ready at {0}", baseAddress);
+                 Console.WriteLine("Press <enter> to stop the service.");
+                 Console.ReadLine();
+
+                 host.Close();
+             }
+        }
+    }
+}
---
+++ SecondService/SecondService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondService
+{
+    public class SecondService : IService.IService
+    {
+        // A dictionary to store the all the UEFA Champions League winners. 
+
+        Dictionary<string, string> winners;
+
+        SecondService()
+        {
+            // very odd to do this inside the constructor , but it's all about the load balancer not this service...
+            try
+            {
+                using (StreamReader file = new StreamReader("Resources/UEFA.json"))
+                {
+                    string winnersJson = file.ReadToEnd();
+                    winners = JsonConvert.DeserializeObject<Dictionary<string, string>>(winnersJson);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("An Exception occured : " + exception.Message);
+            }
+        }
+
+        public string ChampWinner(long year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
