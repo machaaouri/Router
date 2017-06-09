c2113072086fd6d3eb6c0c258e78496885397d59
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-09 17:50:23 +0200

    a simple client that uses channelFactory to connect to the load balancer

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binding = new BasicHttpBinding();
+            var endpoint = new EndpointAddress("http://localhost:8080/FirstService");
+            var channelFactory = new ChannelFactory<IService.IService>(binding, endpoint);
+
+            IService.IService client = null;
+
+            try
+            {
+                client = channelFactory.CreateChannel();
+                client.ChampWinner(1555);
+                ((ICommunicationObject)client).Close();
+            }
+            catch
+            {
+                if (client != null)
+                {
+                    ((ICommunicationObject)client).Abort();
+                }
+            }
+        }
+    }
+}
